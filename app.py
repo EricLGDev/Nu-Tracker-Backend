52156f97718f9285ec6da5da4372bd33fceb028f
Eric <ericlg.link@gmail.com>
2023-02-08 20:26:03 -0800

    Fix db URI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from models import db, UserModel, CalorieIntakeModel, MacroModel
+import config
 
 import os
 
@@ -13,7 +14,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config.from_pyfile('config.cfg')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Initialize Database
 
