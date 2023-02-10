dbf3ba6890cfdb5001f9058e423f21e50bf48fa2
Eric <ericlg.link@gmail.com>
2023-02-10 14:45:32 -0800

    Create the flask app

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Load the configuration from the instance folder
+    app.config.from_pyfile('config.py')
+    
+    # Initialize the database and migrations
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    # Import and register the blueprints
+    from app.routes import bp
+    app.register_blueprint(bp)
+    
+    return app
